7b7cde497d4eebf7c56505756c6b7ca0b9f60491
Himanshu Kumar <himanshu@Himanshus-MacBook-Pro.local>
2026-03-21 17:01:35 +0530

    added form data example thymleaf

--- spring-basic-learning/src/main/java/com/example/spring_basic/learning/controller/HomeController.java
+++ spring-basic-learning/src/main/java/com/example/spring_basic/learning/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,5 +34,30 @@ public class HomeController {
         model.addAttribute("student", student);
         //this will send student object to student.html
         return "student"; // student.html
+    }
+
+    @GetMapping("/students")
+    public String getStudentList(Model model) {
+        StudentDto student1 = StudentDto.builder().id(1L).email("abc@gmail.com").
+                name("Himanshu").courseName("MCA").build();
+        StudentDto student2 = StudentDto.builder().id(2L).email("zyz@gmail.com").
+                name("Kumar").courseName("BCA").build();
+        StudentDto student3 = StudentDto.builder().id(3L).email("123@gmail.com").
+                name("Ranjan").courseName("KCA").build();
+
+        List<StudentDto> studentDtoList=new ArrayList<>();
+        studentDtoList.add(student1);
+        studentDtoList.add(student2);
+        studentDtoList.add(student3);
+        model.addAttribute("students", studentDtoList);
+        //this will send students list to students.html
+        return "students"; // students.html
+    }
+
+    @GetMapping("/student-form")
+    public String showForm(Model model) {
+        model.addAttribute("student", StudentDto.builder().id(1L).
+                courseName("MCA").email("abc@email.com").name("Himanshu").build());
+        return "student-form";
     }
 }
