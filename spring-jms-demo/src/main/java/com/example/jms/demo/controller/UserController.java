b2d76d64db975beecf4405123c70af8796e32e5b
Himanshu Kumar <himanshu@Himanshus-MacBook-Pro.local>
2026-03-27 22:25:25 +0530

    added swagger open api docs

--- spring-jms-demo/src/main/java/com/example/jms/demo/controller/UserController.java
+++ spring-jms-demo/src/main/java/com/example/jms/demo/controller/UserController.java
@@ -2,9 +2,13 @@ package com.example.jms.demo.controller;
 
 import com.example.jms.demo.dto.User;
 import com.example.jms.demo.messaging.UserProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User Controller", description = "APIs for User management")
 @RestController
 public class UserController {
 
@@ -15,8 +19,9 @@ public class UserController {
         this.userProducer = userProducer;
     }
 
+    @Operation(summary = "Testing of user message using JMS", description = "Sending user object Java Messaging services using JMS API")
     @PostMapping("/send-user")
-    public String sendUser(@RequestBody User user) {
+    public @ApiResponse(description = "String Object") String sendUser(@RequestBody User user) {
         userProducer.sendUser(user);
         return "User message sent!";
     }
